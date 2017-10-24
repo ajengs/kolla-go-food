adadf3d1fdfb42b78a57d68f9e1acb77be18268b
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2017-10-24 17:18:18 +0700

    validate buyer & rspec config

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,13 @@
 class Buyer < ApplicationRecord
+  # email format, phone number format : angka <= 12 chars
+  validates :email, :name, :phone, :address, presence: true
+  validates :email, uniqueness: true, format: {
+    with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+    message: 'Email not valid'
+  }
+  validates :phone, length: { is: 12 }, numericality: true
+  # format: {
+  #   with: /\A[+-]?\d+\z/,
+  #   message: 'Phone number not valid'
+  # }
 end
