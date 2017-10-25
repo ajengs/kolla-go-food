e9697f59703cad87ba0eff90869aa7d0610f2d06
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2017-10-25 13:18:52 +0700

    test with Factory

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,82 +1,55 @@
 require 'rails_helper'
 
 describe Food do
-  describe 'Check field validity' do
-    before :each do
-      @food = Food.new(
-        name: 'Sate Ayam',
-        description: 'Sate yang dibuat dari daging ayam',
-        price: 30000.0
-      )
-    end
 
-    context 'with valid name and description' do
-      it 'is valid with a name and description' do
-        expect(@food).to be_valid
-      end
-    end
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:food)).to be_valid
+  end
 
-    context 'without valid name' do
-      it 'is invalid without a name' do
-        @food.name = nil
-        @food.valid?
-        expect(@food.errors[:name]).to include("can't be blank")
-      end
-    end
+  it 'is valid with a name and description' do
+    expect(FactoryGirl.build(:food)).to be_valid
+  end
 
-    context 'without valid description' do
-      it 'is invalid without a description' do
-        @food.description = nil
-        @food.valid?
-        expect(@food.errors[:description]).to include("can't be blank")
-      end
-    end
+  it 'is invalid without a name' do
+    food = FactoryGirl.build(:food, name: nil)
+    food.valid?
+    expect(food.errors[:name]).to include("can't be blank")
+  end
 
-    context 'without valid price' do
-      it 'is a valid with numeric price' do
-        @food.price = '5ooo'
-        @food.valid?
-        expect(@food.errors[:price]).to include("is not a number")
-      end
-    end
+  it 'is invalid without a description' do
+    food = FactoryGirl.build(:food, description: nil)
+    food.valid?
+    expect(food.errors[:description]).to include("can't be blank")
+  end
 
-    context 'with price >= 0.01' do
-      it 'is valid with price >= 0.01' do
-        @food.price = 0.01
-        @food.valid?
-        expect(@food).to be_valid
-      end
-    end
+  it 'is a valid with numeric price' do
+    food = FactoryGirl.build(:food, price: '5ooo')
+    food.valid?
+    expect(food.errors[:price]).to include("is not a number")
+  end
+
+  it 'is valid with price >= 0.01' do
+    food = FactoryGirl.build(:food, price: 0.01)
+    food.valid?
+    expect(food).to be_valid
+  end
+  
+  it 'is invalid with a price < 0.01' do
+    food = FactoryGirl.build(:food, price: -10)
+    food.valid?
+    expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
+  end
     
-    context 'with price < 0.01' do
-      it 'is invalid with a price < 0.01' do
-        @food.price = -10.0
-        @food.valid?
-        expect(@food.errors[:price]).to include("must be greater than or equal to 0.01")
-      end
-    end
-      
-    context "with image ends with something other than '.gif, '.jpg', '.png'" do
-      it "is invalid with image ends with other than '.gif, '.jpg', '.png'" do
-        @food.image_url = 'text.csv'
-        @food.valid?
-        expect(@food.errors[:image_url]).to include("must be a URL for GIF, JPG or PNG image")
-      end
-    end
+  it "is invalid with file ends with other than '.gif, '.jpg', '.png'" do
+    food = FactoryGirl.build(:food, image_url: 'text.csv')
+    food.valid?
+    expect(food.errors[:image_url]).to include("must be a URL for GIF, JPG or PNG image")
   end
 
   it 'is invalid with a duplicate name' do
-    food1 = Food.create(
-      name: 'Sate Ayam',
-      description: 'Sate yang dibuat dari daging ayam',
-      price: 30000.0
-    )
+    food1 = FactoryGirl.create(:food)
 
-    food2 = Food.new(
-      name: 'Sate Ayam',
-      description: 'Sate yang dibuat dari daging ayam loh',
-      price: 30000.0
-    )
+    food2 = FactoryGirl.build(:food)
 
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
@@ -84,19 +57,15 @@ describe Food do
 
   describe 'Filter name by letter' do
     before :each do
-      @food1 = Food.create(
-        name: 'Nasi Uduk',
-        description: 'Betawi style steamed rice cooked in coconut milk',
-        price: 10000.0
-      )
+      @food1 = FactoryGirl.create(:food)
 
-      @food2 = Food.create(
+      @food2 = FactoryGirl.create(:food,
         name: 'Kerak Telor',
         description: 'Betawi traditional spicy omelette',
         price: 8000.0
       )
 
-      @food3 = Food.create(
+      @food3 = FactoryGirl.create(:food,
         name: 'Nasi Semur',
         description: 'Based on dongfruit',
         price: 9000.0
