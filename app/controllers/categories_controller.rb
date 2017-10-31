8a5dd3d800bd06ac8d36f063d1791d73b54ba020
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2017-10-31 09:02:29 +0700

    -

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1.json
   def show
     @foods = Food.by_category(@category.id)
+    # @drinks = Drink.by_category(@category.id)
   end
   
   # GET /foods/new
