58302187239ecbe1a4de11a01c20c05765c54c0d
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2017-11-02 17:06:17 +0700

    session login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin/index', as: 'admin'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
   resources :categories, :orders, :users
   resources :carts
   resources :line_items
